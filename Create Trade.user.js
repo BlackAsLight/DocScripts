ea07f186a3e129378c065dd59fbc549222c17bfe
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2021-02-17 21:37:35 +1100

    Add Create Trade Script v0.1

---
+++ Create Trade.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Doc: Create Trade
+// @namespace    https://politicsandwar.com/nation/id=19818
+// @version      0.1
+// @description  Makes script, View Trades, Outbid and Match buttons work.
+// @author       BlackAsLight
+// @match        https://politicsandwar.com/nation/trade/create/*
+// @grant        none
+// ==/UserScript==
+
+'use strict';
+
+let args = window.location.search.slice(1).split('&');
+for (let i = 0; i < args.length; i++) {
+    args[i] = args[i].split('=');
+    if (args[i][0] == 'p') {
+        document.getElementById('priceper').value = parseInt(args[i][1]);
+    }
+    else if (args[i][0] == 'q') {
+        document.getElementById('amount').value = parseInt(args[i][1]);
+    }
+    else if (args[i][0] == 't') {
+        if (args[i][1] == 's') {
+            document.getElementsByClassName('nationtable')[0].children[0].children[7].children[0].children[0].children[1].style.display = 'none';
+            document.getElementsByClassName('nationtable')[0].children[0].children[7].children[0].children[0].children[0].style.borderRadius = '6px';
+        }
+        else {
+            document.getElementsByClassName('nationtable')[0].children[0].children[7].children[0].children[0].children[0].style.display = 'none';
+            document.getElementsByClassName('nationtable')[0].children[0].children[7].children[0].children[0].children[1].style.borderRadius = '6px';
+        }
+    }
+}
