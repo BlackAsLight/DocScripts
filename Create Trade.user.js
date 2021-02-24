b2784cf1ab96c879fa092f1aa735188ebc7f7a96
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2021-02-24 23:23:25 +1100

    v0.2 Updated script to work with page changes

--- Create Trade.user.js
+++ Create Trade.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Doc: Create Trade
 // @namespace    https://politicsandwar.com/nation/id=19818
-// @version      0.1
+// @version      0.2
 // @description  Makes script, View Trades, Outbid and Match buttons work.
 // @author       BlackAsLight
 // @match        https://politicsandwar.com/nation/trade/create/*
@@ -21,12 +21,12 @@ for (let i = 0; i < args.length; i++) {
     }
     else if (args[i][0] == 't') {
         if (args[i][1] == 's') {
-            document.getElementsByClassName('nationtable')[0].children[0].children[7].children[0].children[0].children[1].style.display = 'none';
-            document.getElementsByClassName('nationtable')[0].children[0].children[7].children[0].children[0].children[0].style.borderRadius = '6px';
+            document.getElementsByClassName('nationtable')[0].children[0].children[9].children[0].children[0].children[1].style.display = 'none';
+            document.getElementsByClassName('nationtable')[0].children[0].children[9].children[0].children[0].children[0].style.borderRadius = '6px';
         }
         else {
-            document.getElementsByClassName('nationtable')[0].children[0].children[7].children[0].children[0].children[0].style.display = 'none';
-            document.getElementsByClassName('nationtable')[0].children[0].children[7].children[0].children[0].children[1].style.borderRadius = '6px';
+            document.getElementsByClassName('nationtable')[0].children[0].children[9].children[0].children[0].children[0].style.display = 'none';
+            document.getElementsByClassName('nationtable')[0].children[0].children[9].children[0].children[0].children[1].style.borderRadius = '6px';
         }
     }
 }
