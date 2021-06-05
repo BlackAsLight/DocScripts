67d617dd8a47a4a3c9c1c2cac3b3cbce008ae63a
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2021-06-05 19:35:01 +1000

    v0.1 Espionage Report

---
+++ Espionage Report/Client.user.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name         Doc: Espionage Report
+// @namespace    https://politicsandwar.com/nation/id=19818
+// @version      0.1
+// @description  Send a Copy of the Espionage Report to a Google Spreadsheet
+// @author       BlackAsLight
+// @match        https://politicsandwar.com/nation/espionage/*
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+'use strict';
+
+// Adds button to insert or update the URL to report espionage operations to.
+(() => {
+	let codeTag = document.createElement('code');
+	codeTag.innerHTML = localStorage.Doc_EspionageReportURL == undefined || localStorage.Doc_EspionageReportURL == '' ? '<button>Insert Report URL</button>' : '<button>Update Report URL</button>';
+	codeTag.onclick = () => {
+		let response = prompt('Espionage Report URL:', localStorage.Doc_EspionageReportURL);
+		if (response != undefined) {
+			localStorage.Doc_EspionageReportURL = response;
+		}
+	};
+	document.getElementById('leftcolumn').appendChild(codeTag);
+})();
+
+// Adds button to insert or update your Identifier Key to report espionage operations.
+(() => {
+	let codeTag = document.createElement('code');
+	codeTag.innerHTML = localStorage.Doc_EspionageIdentifierKey == undefined || localStorage.Doc_EspionageIdentifierKey == '' ? '<button>Insert Identifier Key</button>' : '<button>Update Identifier Key</button>';
+	codeTag.onclick = () => {
+		let response = prompt('Espionage Identifier Key:', localStorage.Doc_EspionageIdentifierKey);
+		if (response != undefined) {
+			localStorage.Doc_EspionageIdentifierKey = response;
+		}
+	};
+	document.getElementById('leftcolumn').appendChild(codeTag);
+})();
+
+// Gets Report and sends it to spreadsheet if all arguments are met.
+let pTag = document.getElementById('result_copy');
+if (pTag != undefined) {
+	if (localStorage.Doc_EspionageReportURL != undefined && localStorage.Doc_EspionageReportURL != '') {
+		if (localStorage.Doc_EspionageIdentifierKey != undefined && localStorage.Doc_EspionageIdentifierKey != '') {
+			sendPost(localStorage.Doc_EspionageReportURL, {
+				timeStamp: new Date(),
+				report: pTag.textContent,
+				identifierKey: localStorage.Doc_EspionageIdentifierKey
+			});
+		}
+	}
+}
+
+// Send a POST request.
+function sendPost(url, jsonData) {
+	GM_xmlhttpRequest({
+		method: 'POST',
+		data: JSON.stringify(jsonData),
+		url: url
+	});
+}
---
+++ Espionage Report/Server.js
@@ -0,0 +1,46 @@
+// ==GoogleScriptApp==
+// @name         Doc: Espionage Report
+// @namespace    https://politicsandwar.com/nation/id=19818
+// @version      0.1
+// @description  Receives a Copy of Espionage Reports and saves to a Google Spreadsheet
+// @author       BlackAsLight
+// ==/GoogleScriptApp==
+
+const spreadsheetID = '';
+const sheetName = 'Reports';
+let sheet = SpreadsheetApp.openById(spreadsheetID).getSheetByName(sheetName);
+
+function doPost(e) {
+	const data = JSON.parse(e.postData.contents);
+	const rowNumber = findRow();
+	sheet.getRange(rowNumber, 1).setValue(data.timeStamp);
+	sheet.getRange(rowNumber, 2).setValue(data.report);
+	sheet.getRange(rowNumber, 3).setValue(data.identifierKey);
+}
+
+function findRow() {
+	if (sheet.getRange(sheet.getMaxRows() - 1, 1).getValue() != '') {
+		sheet.insertRows(sheet.getMaxRows());
+	}
+	const max = sheet.getMaxRows();
+	let min = 2;
+	let position = min;
+	let find = true;
+	let count = 0;
+	while (find && count < max) {
+		if (sheet.getRange(position, 1).getValue() == '') {
+			if (sheet.getRange(position - 1, 1).getValue() != '') {
+				find = false;
+			}
+			else {
+				position = Math.floor((min + position) / 2);
+			}
+		}
+		else {
+			min = position;
+			position = Math.floor((position + max) / 2);
+		}
+		count++;
+	}
+	return position;
+}
