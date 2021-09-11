02d102a9e317de0c73bef4745c7e712ec6fbed64
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2021-09-11 15:14:52 +1000

    v0.2 Reaching Max Ads
    Added Support for when you reach your Max ads.

--- Reward-Ads/README.md
+++ Reward-Ads/README.md
@@ -2,4 +2,4 @@
 
 ## Description
 
-This script runs on the [Rewarded Ads](https://politicsandwar.com/rewarded-ads/) page and simply clicks the button so you don't have to.
+This script runs on the [Rewarded Ads](https://politicsandwar.com/rewarded-ads/) page and simply clicks the button so you don't have to. When you reach the maximum of 25 ads, it forwards you to your Nation page.
--- Reward-Ads/Reward-Ads.user.js
+++ Reward-Ads/Reward-Ads.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Doc: Reward-Ads
 // @namespace    https://politicsandwar.com/nation/id=19818
-// @version      0.1
+// @version      0.2
 // @description  Autoplay Reward Ads
 // @author       BlackAsLight
 // @match        https://politicsandwar.com/rewarded-ads/
@@ -18,9 +18,16 @@ const observer = new MutationObserver((list) => {
 				if (mutation.attributeName == 'style') {
 					// If "btnAds" button shows up again...
 					if (mutation.target.style.display != 'none') {
-						// Click said button.
-						mutation.target.click();
-						console.log('Clicked!');
+						// Then check if we've hit the max for today...
+						if (document.getElementById('rewarded_ads_watched_today').textContent == '25') {
+							// If so then go to our Nation page.
+							window.location = document.getElementsByClassName('sidebar')[1].getElementsByTagName('a')[0].href;
+						}
+						else {
+							// If not then click button.
+							mutation.target.click();
+							console.log('Clicked!');
+						}
 					}
 				}
 			}
@@ -31,11 +38,20 @@ const observer = new MutationObserver((list) => {
 // Starts observing attribute changes for the "btnAds" button.
 observer.observe(document.getElementById('btnAds'), { attributes: true, childList: false, subtree: false });
 
-// Click "btnAds button if needed when page loads.
+// Three second delay to give page time to decide if the button can be clicked or should be hidden.
 setTimeout(() => {
-	const adTag = document.getElementById('btnAds');
-	if (adTag.style.display != 'none') {
-		adTag.click();
-		console.log('Clicked!');
+	// Check if we've hit the max for today...
+	if (document.getElementById('rewarded_ads_watched_today').textContent == '25') {
+		// If so then go to our Nation page.
+		window.location = document.getElementsByClassName('sidebar')[1].getElementsByTagName('a')[0].href;
+	}
+	else {
+		// If not then if button is displayed...
+		const adTag = document.getElementById('btnAds');
+		if (adTag.style.display != 'none') {
+			// Click button.
+			adTag.click();
+			console.log('Clicked!');
+		}
 	}
 }, 3000);
