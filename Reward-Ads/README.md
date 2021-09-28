ea08ec0c4a5096fa582c87931fdfc57a4c2e786e
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2021-09-28 15:56:32 +1000

    v0.6 Aggressive Mode is back!

--- Reward-Ads/README.md
+++ Reward-Ads/README.md
@@ -3,3 +3,7 @@
 ## Description
 
 This script runs on the [Rewarded Ads](https://politicsandwar.com/rewarded-ads/) page and clicks the button so you don't have to. When you reach the maximum of 25 ads, it forwards you to your Nation page. It will work in it's own tab, but a window is suggested as the browser seems to pause or slow down JavaScript when inactive.
+
+### Aggressive Mode
+
+At the bottom of the left sidebar you'll find a checkbox to toggle AggressiveMode on and off. If on, the script will keep it's own timer and force the button to reappear when it says 3 mins is up. If left off, it will just wait for the game to make the button reappear. The game's timer can count slow at times making what should only take a max of an hour and 15 mins much longer.
--- Reward-Ads/Reward-Ads.user.js
+++ Reward-Ads/Reward-Ads.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Doc: Reward-Ads
 // @namespace    https://politicsandwar.com/nation/id=19818
-// @version      0.5
+// @version      0.6
 // @description  Autoplay Reward Ads
 // @author       BlackAsLight
 // @match        https://politicsandwar.com/rewarded-ads/
@@ -29,6 +29,36 @@ const observer = new MutationObserver((list) => {
 							console.log('Clicked!');
 						}
 					}
+					// If "btnAds" button disappeared, and AggressiveMode is on...
+					else if (localStorage.Doc_RewardAds == 'true') {
+						console.log('Aggressive Mode is Active!');
+						// Check if time until next ad needs to be updated.
+						if (new Date(parseInt(localStorage.Doc_RewardAdsTimer)) < new Date()) {
+							console.log('Changing: ' + localStorage.Doc_RewardAdsTimer);
+							localStorage.Doc_RewardAdsTimer = new Date().getTime() + 1000 * 60 * 3;
+						}
+
+						// Set interval to check every second if timer is up.
+						setInterval(() => {
+							const ticks = parseInt(localStorage.Doc_RewardAdsTimer) - new Date().getTime();
+							console.log(ticks);
+							// If timer is up reset the page to run another ad.
+							if (ticks < 0) {
+								console.log('Resetting!');
+
+								// Hide Stuff.
+								document.getElementById('ad-watched').style.display = 'none';
+								document.getElementById('countdown').style.display = 'none';
+
+								// Display the "btnAds" button.
+								document.getElementById('btnAds').style.display = '';
+								console.log('Reset!');
+
+								// Clear all existing intervals.
+								clearIntervals();
+							}
+						}, 1000);
+					}
 				}
 			}
 		}
@@ -38,12 +68,11 @@ const observer = new MutationObserver((list) => {
 // Start observing attribute changes for the "btnAds" button.
 observer.observe(document.getElementById('btnAds'), { attributes: true, childList: false, subtree: false });
 
-// Remove Old localStorage
-if (localStorage.Doc_RewardAds != undefined) {
-	localStorage.removeItem('Doc_RewardAds');
-}
-
 setTimeout(() => {
+	if (localStorage.Doc_RewardAdsTimer == undefined) {
+		localStorage.Doc_RewardAdsTimer = new Date().getTime();
+	}
+
 	// Check if we've hit the max for today...
 	if (document.getElementById('rewarded_ads_watched_today').textContent == '25') {
 		// If so then go to our Nation page.
@@ -58,4 +87,28 @@ setTimeout(() => {
 			console.log('Clicked!');
 		}
 	}
-}, 1500);
+}, 2000);
+
+{
+	// Get User input on whether or not to set Aggressive Mode on.
+	const codeTag = document.createElement('code');
+	codeTag.innerHTML = `Aggressive Mode: <input id="aggressiveMode" type="checkbox" ${localStorage.Doc_RewardAds == 'true' ? 'checked' : ''}>`;
+	document.getElementById('leftcolumn').appendChild(codeTag);
+	document.getElementById('aggressiveMode').onchange = () => {
+		const inputTag = document.getElementById('aggressiveMode');
+		if (inputTag.checked) {
+			localStorage.Doc_RewardAds = true;
+			window.location.reload();
+		}
+		else {
+			localStorage.removeItem('Doc_RewardAds');
+		}
+	};
+}
+
+function clearIntervals() {
+	const id = setTimeout(() => { console.log('Cleared Old Timers!') }, 1000);
+	for (let i = 0; i < id; ++i) {
+		clearInterval(i);
+	}
+}
