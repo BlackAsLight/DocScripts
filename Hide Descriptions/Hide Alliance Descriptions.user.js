61559d0fcc055323fcb42595b73c05b7ef2a0796
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2021-10-17 16:00:40 +1100

    v0.2 Hide Alliance Descriptions
    Buttons to toggle alliance description only show up if the alliance actually has a description.

--- Hide Descriptions/Hide Alliance Descriptions.user.js
+++ Hide Descriptions/Hide Alliance Descriptions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Doc: Hide Alliance Descriptions
 // @namespace    https://politicsandwar.com/nation/id=19818
-// @version      0.1
+// @version      0.2
 // @description  Hides Alliances's Descriptions set up by their government. Why? Because some people like to make them excessively long.
 // @author       BlackAsLight
 // @match        https://politicsandwar.com/alliance/id=*
@@ -12,43 +12,45 @@
 'use strict';
 const contTag = document.getElementsByClassName('ck-content')[0];
 
-// Insert button to toggle between Show/Hide Alliance's Description.
-contTag.parentElement.insertBefore((() => {
-	const divTag = document.createElement('div');
-	divTag.style.display = 'block';
-	divTag.style.paddingBottom = '1em';
-	divTag.style.textAlign = 'center';
-	divTag.appendChild((() => {
-		const buttonTag = document.createElement('button');
-		buttonTag.id = 'Doc_Button';
-		buttonTag.style.backgroundColor = '#337AB7';
-		buttonTag.style.color = '#FFFFFF';
-		buttonTag.style.padding = '1em';
-		buttonTag.style.borderRadius = '6px';
-		buttonTag.innerText = 'Hide Description!';
-		buttonTag.onclick = buttonClick;
-		return buttonTag;
-	})());
-	return divTag;
-})(), contTag);
+if (contTag.childElementCount) {
+	// Insert button to toggle between Show/Hide Alliance's Description.
+	contTag.parentElement.insertBefore((() => {
+		const divTag = document.createElement('div');
+		divTag.style.display = 'block';
+		divTag.style.paddingBottom = '1em';
+		divTag.style.textAlign = 'center';
+		divTag.appendChild((() => {
+			const buttonTag = document.createElement('button');
+			buttonTag.id = 'Doc_Button';
+			buttonTag.style.backgroundColor = '#337AB7';
+			buttonTag.style.color = '#FFFFFF';
+			buttonTag.style.padding = '1em';
+			buttonTag.style.borderRadius = '6px';
+			buttonTag.innerText = 'Hide Description!';
+			buttonTag.onclick = buttonClick;
+			return buttonTag;
+		})());
+		return divTag;
+	})(), contTag);
 
-// Insert second button to Hide Alliance's Description.
-contTag.appendChild((() => {
-	const divTag = document.createElement('div');
-	divTag.style.paddingBottom = '1em';
-	divTag.style.textAlign = 'center';
-	divTag.appendChild((() => {
-		const buttonTag = document.createElement('button');
-		buttonTag.style.backgroundColor = '#337AB7';
-		buttonTag.style.color = '#FFFFFF';
-		buttonTag.style.padding = '1em';
-		buttonTag.style.borderRadius = '6px';
-		buttonTag.innerText = 'Hide Description!';
-		buttonTag.onclick = buttonClick;
-		return buttonTag;
+	// Insert second button to Hide Alliance's Description.
+	contTag.appendChild((() => {
+		const divTag = document.createElement('div');
+		divTag.style.paddingBottom = '1em';
+		divTag.style.textAlign = 'center';
+		divTag.appendChild((() => {
+			const buttonTag = document.createElement('button');
+			buttonTag.style.backgroundColor = '#337AB7';
+			buttonTag.style.color = '#FFFFFF';
+			buttonTag.style.padding = '1em';
+			buttonTag.style.borderRadius = '6px';
+			buttonTag.innerText = 'Hide Description!';
+			buttonTag.onclick = buttonClick;
+			return buttonTag;
+		})());
+		return divTag;
 	})());
-	return divTag;
-})());
+}
 
 // Function to Switch between Displaying and Hiding the Nation's Description.
 function buttonClick() {
