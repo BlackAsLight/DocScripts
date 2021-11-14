9fbb0b3d796715951c6fea50901ef1d2706a2d0c
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2021-11-14 16:01:19 +1100

    v0.5 Added Support for Game's Hide Nation Description Button

--- Hide Descriptions/Hide Nation Descriptions.user.js
+++ Hide Descriptions/Hide Nation Descriptions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Doc: Hide Nation Descriptions
 // @namespace    https://politicsandwar.com/nation/id=19818
-// @version      0.4
+// @version      0.5
 // @description  Hides Nation's Descriptions set up by the user. Why? Because some people like to make them excessively long.
 // @author       BlackAsLight
 // @match        https://politicsandwar.com/nation/id=*
@@ -10,59 +10,62 @@
 // ==/UserScript==
 
 'use strict';
-const descTag = document.getElementById('descCollapseDiv');
+const descDivTag = document.getElementById('descCollapseDiv');
 
 // Hide Nation Description By Default.
-descTag.style.display = 'none';
+descDivTag.style.display = 'none';
 
 // Only insert buttons if the Nation actually has a Description.
-if (descTag.children[0].childElementCount) {
+if (descDivTag.children[0].childElementCount) {
 	// Insert button to toggle between Show/Hide Nation's Description.
-	descTag.parentElement.insertBefore((() => {
+	descDivTag.parentElement.insertBefore((() => {
 		const divTag = document.createElement('div');
 		divTag.style.display = 'block';
 		divTag.style.paddingBottom = '1em';
 		divTag.style.textAlign = 'center';
 		divTag.appendChild((() => {
-			const buttonTag = document.createElement('button');
+			const buttonTag = CreateButton('Show Description!');
 			buttonTag.id = 'Doc_Button';
-			buttonTag.style.backgroundColor = '#337AB7';
-			buttonTag.style.color = '#FFFFFF';
-			buttonTag.style.padding = '1em';
-			buttonTag.style.borderRadius = '6px';
-			buttonTag.innerText = 'Show Description!';
-			buttonTag.onclick = buttonClick;
 			return buttonTag;
 		})());
 		return divTag;
-	})(), descTag);
+	})(), descDivTag);
 
 	// Insert second button to Hide Nation's Description.
-	descTag.insertBefore((() => {
+	descDivTag.insertBefore((() => {
 		const divTag = document.createElement('div');
 		divTag.style.textAlign = 'center';
-		divTag.appendChild((() => {
-			const buttonTag = document.createElement('button');
-			buttonTag.style.backgroundColor = '#337AB7';
-			buttonTag.style.color = '#FFFFFF';
-			buttonTag.style.padding = '1em';
-			buttonTag.style.borderRadius = '6px';
-			buttonTag.innerText = 'Hide Description!';
-			buttonTag.onclick = buttonClick;
-			return buttonTag;
-		})());
+		divTag.appendChild(CreateButton('Hide Description!'));
 		return divTag;
-	})(), descTag.children[1]);
+	})(), descDivTag.children[1]);
+}
+
+// Remove Game's Nation Description Hide Button if User has it enabled.
+const descButtonTag = document.getElementById('descCollapseBtn');
+if (descButtonTag) {
+	descButtonTag.remove();
+}
+
+// Function to Create Button to Show and Hide Nation's Description.
+function CreateButton(text) {
+	const buttonTag = document.createElement('button');
+	buttonTag.style.backgroundColor = '#337AB7';
+	buttonTag.style.color = '#FFFFFF';
+	buttonTag.style.padding = '1em';
+	buttonTag.style.borderRadius = '6px';
+	buttonTag.innerText = text;
+	buttonTag.onclick = ButtonClick;
+	return buttonTag;
 }
 
 // Function to Switch between Displaying and Hiding the Nation's Description.
-function buttonClick() {
-	if (descTag.style.display == 'none') {
+function ButtonClick() {
+	if (descDivTag.style.display == 'none') {
 		document.getElementById('Doc_Button').innerText = 'Hide Description!';
-		descTag.style.display = '';
+		descDivTag.style.display = '';
 	}
 	else {
 		document.getElementById('Doc_Button').innerText = 'Show Description!';
-		descTag.style.display = 'none';
+		descDivTag.style.display = 'none';
 	}
 }
