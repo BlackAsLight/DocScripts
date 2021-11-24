37de934b0face5c29d2d7c13dc2ade7b32ced22c
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2021-11-24 14:52:55 +1100

    0.2 No More Refresh
    Made it so you no longer need to refresh the page. It checks in the background dynamically.

--- Baseball/Home Games.user.js
+++ Baseball/Home Games.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Doc: Home Baseball
 // @namespace    https://politicsandwar.com/nation/id=19818
-// @version      0.1
+// @version      0.2
 // @description  Make Hosting Games Better
 // @author       BlackAsLight
 // @match        https://politicsandwar.com/obl/host/
@@ -10,11 +10,130 @@
 // ==/UserScript==
 
 'use strict';
-window.onload = () => {
-	// Refresh page if currently hosting a game.
-	if (Array.from(document.getElementsByTagName('input')).filter(x => x.type == 'submit')[0].value == 'Cancel Home Game') {
-		setTimeout(() => {
-			window.location = 'https://politicsandwar.com/obl/host/';
-		}, 3000);
+let token = Array.from(document.getElementsByTagName('input')).filter(x => x.name == 'token')[0].value;
+let isHosting = Array.from(document.getElementsByTagName('input')).filter(x => x.name == 'cancelhomegame')[0] ? true : false;
+let toggling = false;
+let checking = false;
+
+function SetUp() {
+	const divTag = (() => {
+		const divTag = document.createElement('div');
+		const formTag = Array.from(document.getElementsByTagName('input')).filter(x => x.name == 'token')[0].parentElement;
+		formTag.parentElement.insertBefore(divTag, formTag);
+		formTag.remove();
+		return divTag;
+	})();
+	divTag.style.textAlign = 'center';
+	divTag.style.fontSize = '1.25em';
+	divTag.appendChild((() => {
+		const buttonTag = document.createElement('button');
+		buttonTag.id = 'HOST';
+		buttonTag.innerText = 'Host Game';
+		buttonTag.className = 'btn';
+		buttonTag.style.padding = '0.5em';
+		buttonTag.style.backgroundColor = '#2648DA';
+		buttonTag.style.color = '#FFFFFF';
+		buttonTag.style.fontSize = 'inherit';
+		buttonTag.style.borderRadius = '0.5em';
+		buttonTag.disabled = isHosting;
+		buttonTag.onclick = async () => {
+			await ToggleGameStatus(buttonTag);
+			isHosting = true;
+			CheckGameStatus();
+		};
+		return buttonTag;
+	})());
+	divTag.append(' | ');
+	divTag.appendChild((() => {
+		const buttonTag = document.createElement('button');
+		buttonTag.id = 'CANCEL';
+		buttonTag.innerText = 'Cancel Game';
+		buttonTag.className = 'btn';
+		buttonTag.style.padding = '0.5em';
+		buttonTag.style.backgroundColor = '#D9534F';
+		buttonTag.style.color = '#FFFFFF';
+		buttonTag.style.fontSize = 'inherit';
+		buttonTag.style.borderRadius = '0.5em';
+		buttonTag.disabled = !isHosting;
+		buttonTag.onclick = () => {
+			ToggleGameStatus(buttonTag);
+		};
+		return buttonTag;
+	})());
+}
+
+async function ToggleGameStatus(buttonTag) {
+	buttonTag.disabled = true;
+	while (checking) {
+		console.log('Checking...');
+		buttonTag.disabled = true;
+		await Sleep(1000);
+	}
+	toggling = true;
+	await fetch(location.href, {
+		method: 'POST',
+		body: (() => {
+			let formData = new FormData();
+			formData.append('token', token);
+			if (isHosting) {
+				formData.append('cancelhomegame', 'Cancel Home Game');
+			}
+			else {
+				formData.append('submithomegame', 'Host Home Game');
+			}
+			return formData;
+		})()
+	});
+	toggling = false;
+	console.log('Toggled');
+}
+
+async function CheckGameStatus() {
+	try {
+		let checks = 1;
+		console.log(`Sleep Time: ${Math.floor(checks / 10) + 1}s`);
+		while (isHosting) {
+			checking = true;
+			console.time('Ping');
+			const doc = new DOMParser().parseFromString(await (await fetch(location.href)).text(), 'text/html');
+			console.timeEnd('Ping');
+			token = Array.from(doc.getElementsByTagName('input')).filter(x => x.name == 'token')[0].value;
+			isHosting = (() => {
+				const inputTags = Array.from(doc.getElementsByTagName('input')).filter(x => x.name == 'cancelhomegame' || x.name == 'submithomegame');
+				if (inputTags.length) {
+					return inputTags[0].name == 'cancelhomegame';
+				}
+				throw Error();
+			})();
+			document.getElementById('HOST').disabled = isHosting;
+			document.getElementById('CANCEL').disabled = !isHosting;
+			checking = false;
+			if (isHosting) {
+				if (!(checks % 10) && checks <= 90) {
+					console.log(`Sleep Time: ${Math.floor(checks / 10) + 1}s`);
+				}
+				await Sleep(Math.min(Math.floor(checks / 10) * 1000 + 1000, 10000));
+				++checks;
+			}
+			while (toggling) {
+				console.log('Toggling...');
+				await Sleep(1000);
+			}
+		}
+		console.log('No Longer Hosting.');
+	}
+	catch {
+		location.href = 'https://politicsandwar.com/human/';
 	}
 }
+
+async function Sleep(ms) {
+	await new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(true);
+		}, ms);
+	});
+}
+
+SetUp();
+CheckGameStatus();
--- Baseball/README.md
+++ Baseball/README.md
@@ -4,7 +4,14 @@
 
 ### Away Games
 
-This script runs on the [Baseball Away Game's Page](https://politicsandwar.com/obl/play/), and refreshes the page every 3s when there are no available games until a game becomes available. When a game does get played, it will also hide the game's details so the page looks like it never happened and so you therefore, don't have to scroll.
+This script runs on the [Baseball Away Game's Page](https://politicsandwar.com/obl/play/).
+- It will remove the current Host or Cancel Game button and replace it with it's own which are both displayed at the same time constantly. Blue and Red respectively.
+- Depending on your current status, one of the buttons will be disabled indicating which mode you are in.
+- When you click the blue button to host a game, you will switch modes and the game will now start checking to see when the game if fulfilled to then switch you back, for you to click again.
+- You can click the red button to cancel the game, if you wish to stop without fulfilling the game.
+- The script will sleep between checks. This sleep time changes dynamically, starting at 1s and increases by 1s every 10 checks to a max of 10s sleep time.
+  - This sleep time resets back to 1s if you cancel the game then start it again, or if the game is fulfilled.
+- It is **not recommended** to view the market pages while this script is running, or you might find it a bit hard to accept or create trade offers.
 
 ### Home Games
 
