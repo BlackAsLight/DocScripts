317c5edeba74c7eb3e7cb1f00ee1ad44f42a0d24
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2023-07-02 15:04:20 +1000

    Update Military.ts
    - Added Tanks getting replaced and filled out.

--- src/Military.ts
+++ src/Military.ts
@@ -25,15 +25,18 @@ document.body.append(createTag<HTMLDivElement>('div', divTag => {
 const data: Promise<{
 	soldiers: number,
 	soldiers_today: number,
+	tanks: number,
+	tanks_today: number,
 	cities: {
-		barracks: number
+		barracks: number,
+		factory: number
 	}[]
 }> = fetch(`https://api.politicsandwar.com/graphql?api_key=${LocalStorage.APIKey()}`, {
 	method: 'POST',
 	headers: {
 		'Content-Type': 'application/json'
 	},
-	body: JSON.stringify({ query: '{me{nation{soldiers,soldiers_today,cities{barracks}}}}' })
+	body: JSON.stringify({ query: '{me{nation{soldiers,soldiers_today,tanks,tanks_today,cities{barracks,factory}}}}' })
 })
 	.then(x => x.json())
 	.then(x => x.data.me.nation)
@@ -99,6 +102,52 @@ createTag<HTMLFormElement>('form', formTag => {
 			body: new FormData(this)
 		})
 	}, { passive: false })
+})
+
+// Tanks
+createTag<HTMLFormElement>('form', formTag => {
+	const divTag = document.querySelector<HTMLDivElement>('#rightcolumn>.row')!
+	divTag.parentElement!.insertBefore(formTag, divTag)
+	divTag.remove()
+
+	formTag.append(
+		createTag<HTMLLabelElement>('label', labelTag => labelTag.append(
+			'Tanks Possessed: ', createTag<HTMLSpanElement>('span', spanTag => {
+				spanTag.append('?')
+				data.then(nation => spanTag.textContent = nation.tanks.toString())
+			})
+		)),
+		createTag<HTMLLabelElement>('label', labelTag => labelTag.append(
+			'Tanks Manufactured Today: ', createTag<HTMLSpanElement>('span', spanTag => {
+				spanTag.append('?')
+				data.then(nation => spanTag.textContent = nation.tanks_today.toString())
+			})
+		)),
+		createTag<HTMLLabelElement>('label', labelTag => labelTag.append(
+			'Enlist/Discharge: ', createTag<HTMLInputElement>('input', inputTag => {
+				inputTag.setAttribute('type', 'number')
+				inputTag.setAttribute('name', 'tanks')
+				inputTag.setAttribute('value', '0')
+				data.then(nation => inputTag.value = Math.round(nation.cities.reduce((sum, city) => sum + city.factory, 0) * 50 - nation.tanks_today).toString())
+			})
+		)),
+		createTag<HTMLInputElement>('input', inputTag => {
+			inputTag.setAttribute('type', 'submit')
+			inputTag.setAttribute('name', 'buytanks')
+			inputTag.setAttribute('value', 'Manufacture/Decommission Tanks')
+			inputTag.toggleAttribute('disabled', true)
+			data.then(_nation => inputTag.toggleAttribute('disabled', false))
+		}),
+		createTag<HTMLInputElement>('input', inputTag => {
+			inputTag.setAttribute('type', 'hidden')
+			inputTag.setAttribute('name', 'token')
+			inputTag.setAttribute('value', '')
+		}),
+		createTag<HTMLAnchorElement>('a', aTag => {
+			aTag.setAttribute('href', 'https://politicsandwar.com/nation/military/tanks/')
+			aTag.textContent = 'Go to Page'
+		})
+	)
 })
 
 /* Functions
