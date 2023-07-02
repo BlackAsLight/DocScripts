fc42d25bdcee4279a6bd34e1184f85d19173cd7d
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2023-07-02 15:05:22 +1000

    Create utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,23 @@
+export type None = undefined | null
+// deno-lint-ignore no-explicit-any
+export type Irrelevant = any
+
+export function createTag<T extends HTMLElement>(type: string, func?: ((tag: T) => void)): T {
+	const tag = document.createElement(type) as T
+	if (func)
+		func(tag)
+	return tag
+}
+
+interface LocalStorage {
+	APIKey(): string | null
+	APIKey(set: string | null): void
+}
+export const LocalStorage: LocalStorage = {
+	APIKey(set?: string | null): Irrelevant {
+		return localStorage[ set === undefined ? 'getItem' : set !== null ? 'setItem' : 'removeItem' ]('Doc_APIKey', set!)
+	}
+}
+export enum GetLocalStorageKey {
+	Doc_APIKey = 'APIKey'
+}
