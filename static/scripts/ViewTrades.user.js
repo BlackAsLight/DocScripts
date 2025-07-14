ccdf83e617f418220868fc8a8929bb7e18834af2
leonissenbaum <leonissenbaum@users.noreply.github.com>
2025-07-14 08:34:41 +0600

    Update ViewTrades.user.js

--- static/scripts/ViewTrades.user.js
+++ static/scripts/ViewTrades.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Doc: View Trades
 // @namespace    https://politicsandwar.com/nation/id=19818
-// @version      6.92
+// @version      6.93
 // @description  Make Trading on the market Better!
 // @author       BlackAsLight
 // @include      https://politicsandwar.com/index.php?id=26*
